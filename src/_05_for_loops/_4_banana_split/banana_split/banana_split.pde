62760adf1e78418cc414c8564e7c3861c527c944
root <root@5b13564a39d8>
2022-03-16 01:20:44 +0000

    commit

---
+++ src/_05_for_loops/_4_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,7 @@
+size(500,500);
+String ic = "ice cream ";
+for (int i=0; i < 1; i++){
+  textSize(25);
+  text(ic + ic + ic +"banana", 0, 250 );
+ 
+}
--- src/_06_spot_the_error/MakeItCount.java
+++ src/_06_spot_the_error/MakeItCount.java
@@ -10,8 +10,8 @@ public class MakeItCount {
 	// CODE.
 
 	public static void main(String[] args) {
-		int number = 0;
-		for (int i=1; i<4; i++) {
+		
+		for (int number=1; number<4; number++) {
 			System.out.println(number);
 		}
 	}
